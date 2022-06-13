2b8f61a15d7a84c3244132b19046babec4cad383
John Barry <john.barry@gmail.com>
2022-06-13 14:42:03 -0500

    functools.partial == curry

--- src/auditlog/middleware.py
+++ src/auditlog/middleware.py
@@ -5,7 +5,7 @@ import time
 
 from django.conf import settings
 from django.db.models.signals import pre_save
-from django.utils.functional import curry
+from functools import partial as curry
 from django.apps import apps
 from auditlog.models import LogEntry
 
